d698fa4aca358c66243923f6f2a594ede910844f
Oleg Fedorov <akademuk24@gmail.com>
2022-10-18 14:41:27 +0300

    sss

---
+++ script/slider.js
@@ -0,0 +1,11 @@
+if (window.matchMedia("(max-width: 576px)").matches) {
+    $('.fade').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '10%'
+});
+} 
+
+
